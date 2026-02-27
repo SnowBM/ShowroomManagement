d5773a7ca2abfea84c40014ed03a5c50c7977504
lethanhhaihero-dot <lethanhhaihero@gmail.com>
2026-02-27 19:29:10 +0700

    Thuc su nop code BrandService (da xoa .git con)

---
+++ BrandService/src/main/java/com/mycompany/brandservice/BrandService.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.brandservice;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class BrandService {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
